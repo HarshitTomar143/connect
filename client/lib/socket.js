271add0ccd0ff3e15c05eb05ba0bd985333ce8cf
Harshittomar <tomarharshit2023@gmail.com>
2026-02-28 14:43:57 +0530

    fixing errors

--- client/lib/socket.js
+++ client/lib/socket.js
@@ -2,8 +2,11 @@ import { io } from "socket.io-client";
 
 let socket;
 
+const SOCKET_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+
 export const connectSocket = (token) => {
-  socket = io("http://localhost:5000", {
+  socket = io(SOCKET_URL, {
     auth: { token },
     withCredentials: true,
   });
